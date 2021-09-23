7e4d37246b1c86b7868b31d0e7cbe0e31cc61823
siddharthkushwaha <siddharth.india51088@gmail.com>
2021-09-23 19:45:22 +0530

    logging exception handler

--- src/main/java/com/creditsuisse/covid19/exception/handler/Covid19ResponseEntityExceptionHandler.java
+++ src/main/java/com/creditsuisse/covid19/exception/handler/Covid19ResponseEntityExceptionHandler.java
@@ -2,6 +2,8 @@ package com.creditsuisse.covid19.exception.handler;
 
 import com.mongodb.MongoBulkWriteException;
 import com.mongodb.MongoWriteException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,8 @@ import java.util.Date;
 @ControllerAdvice
 @RestController
 public class Covid19ResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(Covid19ResponseEntityExceptionHandler.class);
 
     private class ExceptionResponseObject{
         private Date timestamp;
@@ -44,12 +48,14 @@ public class Covid19ResponseEntityExceptionHandler extends ResponseEntityExcepti
 
     @ExceptionHandler({Covid19Exception.class})
     public final ResponseEntity handleCovid19Exception(Covid19Exception ex, WebRequest request){
+        logger.error("Exception:", ex.getMessage(), ex);
         ExceptionResponseObject exceptionResponseObject = new ExceptionResponseObject(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponseObject, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler({NotFoundException.class})
     public final ResponseEntity handleNotFoundException(NotFoundException ex, WebRequest request){
+        logger.error("Exception:", ex.getMessage(), ex);
         ExceptionResponseObject exceptionResponseObject = new ExceptionResponseObject(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponseObject, HttpStatus.NOT_FOUND);
     }
@@ -57,20 +63,15 @@ public class Covid19ResponseEntityExceptionHandler extends ResponseEntityExcepti
     //MongoWriteException
     @ExceptionHandler({MongoWriteException.class})
     public final ResponseEntity handleMongoWriteException(MongoWriteException ex, WebRequest request){
-        String exceptionMessage;
+        logger.error("Exception:", ex.getMessage(), ex);
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-        switch(ex.getError().getCode()){
-            case 1100:
-                exceptionMessage = "username or email already exist";
-                break;
-            default:exceptionMessage = ex.getMessage();
-        }
-        ExceptionResponseObject exceptionResponseObject = new ExceptionResponseObject(new Date(), exceptionMessage, request.getDescription(false));
+        ExceptionResponseObject exceptionResponseObject = new ExceptionResponseObject(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponseObject, httpStatus);
     }
 
     @ExceptionHandler({MongoBulkWriteException.class})
     public final ResponseEntity handleMongoBulkWriteException(MongoBulkWriteException ex, WebRequest request){
+        logger.error("Exception:", ex.getMessage(), ex);
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         ExceptionResponseObject exceptionResponseObject = new ExceptionResponseObject(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponseObject, httpStatus);
@@ -78,6 +79,7 @@ public class Covid19ResponseEntityExceptionHandler extends ResponseEntityExcepti
 
     @ExceptionHandler({BadRequestException.class})
     public final ResponseEntity handleBadRequestException(BadRequestException ex, WebRequest request){
+        logger.error("Exception:", ex.getMessage(), ex);
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
         ExceptionResponseObject exceptionResponseObject = new ExceptionResponseObject(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponseObject, httpStatus);
@@ -85,6 +87,7 @@ public class Covid19ResponseEntityExceptionHandler extends ResponseEntityExcepti
 
     //@ExceptionHandler({BindException.class})
     public ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        logger.error("Exception:", ex.getMessage(), ex);
         HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
         ExceptionResponseObject exceptionResponseObject = new ExceptionResponseObject(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponseObject, httpStatus);
